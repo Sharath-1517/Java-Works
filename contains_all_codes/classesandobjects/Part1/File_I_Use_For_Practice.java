6392dfd72d43e56cfc4d16a136a70e9f19b7da03
Sharath_1517 <sharathprof1517@gmail.com>
2022-11-15 19:55:37 +0530

    New Update. Added a bulk update on the file File_I_Use_For_Practice.java

--- contains_all_codes/classesandobjects/Part1/File_I_Use_For_Practice.java
+++ contains_all_codes/classesandobjects/Part1/File_I_Use_For_Practice.java
@@ -1,18 +1,29 @@
 package classesandobjects.Part1;
 
+//As file name specifies this is a file that I use only for learning purposes.
+
 public class File_I_Use_For_Practice {
     public static void main(String[] args) {
 
-        final Students sharath = new Students("My name is sharath");
-        sharath.name = "Sharath Lingam";
+//        final Students sharath = new Students("My name is sharath");
+//        sharath.name = "Sharath Lingam";
 
 //        sharath = new Students("Lingam is my last name.");
 //        I have commented the above line because the finalized objects attributes can be changed but the object itself can't be assigned to another instance.
+
+        Students A = new Students("new Class");
+
+        for (int i = 0; i < 1000000000; i++) {
+            A = new Students("My name");
+        }
+
+//        Lastly used this file for learning how garbage collection is worked and how finalize function is called.
 
     }
 }
 
 
+@SuppressWarnings("removal")
 class Students{
 
     String name;
@@ -22,4 +33,8 @@ class Students{
         this.name = name;
     }
 
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Object is destroyed.");
+    }
 }
